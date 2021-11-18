3dd35691f0f008596881e5d6e3ed0d6d1c86922e
Ramón Huidobro <hola@ramonh.dev>
2021-11-18 23:14:44 +0100

    Test img

--- .eleventy.js
+++ .eleventy.js
@@ -7,8 +7,30 @@ const filters = require('./utils/filters.js')
 const transforms = require('./utils/transforms.js')
 const shortcodes = require('./utils/shortcodes.js')
 
+const Image = require("@11ty/eleventy-img");
+
+async function imageShortcode(src, alt, sizes) {
+  let metadata = await Image(src, {
+    widths: [300, 600],
+    formats: ["avif", "jpeg"]
+  });
+
+  let imageAttributes = {
+    alt,
+    sizes,
+    loading: "lazy",
+    decoding: "async",
+  };
+
+  // You bet we throw an error on missing alt in `imageAttributes` (alt="" works okay)
+  return Image.generateHTML(metadata, imageAttributes);
+}
+
 module.exports = function (config) {
     // Plugins
+    config.addNunjucksAsyncShortcode("image", imageShortcode);
+    config.addLiquidShortcode("image", imageShortcode);
+    config.addJavaScriptFunction("image", imageShortcode);
     config.addPlugin(pluginRss)
     config.addPlugin(pluginNavigation)
     config.addPlugin(pluginSvgSprite, {
