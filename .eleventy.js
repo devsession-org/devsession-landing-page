62a37ce437a7b20df7fc0eab5d6934754b6101aa
Ramón Huidobro <hola@ramonh.dev>
2021-11-19 00:28:41 +0100

    Add image stuff

--- .eleventy.js
+++ .eleventy.js
@@ -12,7 +12,8 @@ const Image = require("@11ty/eleventy-img");
 async function imageShortcode(src, alt, sizes) {
   let metadata = await Image(src, {
     widths: [300, 600],
-    formats: ["avif", "jpeg"]
+    formats: ["avif", "jpeg"],
+    outputDir: "./dist/img/"
   });
 
   let imageAttributes = {
