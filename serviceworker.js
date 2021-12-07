7d5f8ea00a84fc4603fef8c116de6c5a28b19298
ramonh <ramonh@users.noreply.github.com>
2021-12-07 21:20:13 +0000

    deploy: 4ffa7387953d5717ead6306085b36c4e1d1aa32d

--- serviceworker.js
+++ serviceworker.js
@@ -1,5 +1,5 @@
 
-const CACHE_NAME = 'cache-2021-12-07T20:46:15.132'
+const CACHE_NAME = 'cache-2021-12-07T21:19:55.030'
 const OFFLINE_PAGE_URL = '/offline/'
 const PRECACHE_RESOURCES = []
 const IGNORED_HOSTS = ['localhost']
